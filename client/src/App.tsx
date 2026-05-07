ddd20b7846150b128d2519815ec84ad1d7c6ee24
KennBalino <kennbalino@gmail.com>
2026-05-07 14:20:02 +0800

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+
+  return (
+    <div className="flex font-bold justify-center items-center h-screen text-3xl text-blue-800">
+      Hello World
+    </div>
+  )
+}
+
+export default App
